1b950cd2fd546695dec9a01c4e587253eb84f547
Lucas Honorio <lucashonorioo@hotmail.com>
2025-07-20 19:29:27 -0300

    Refatorando metodo Delete em Category Service, e nova Exceção

--- src/main/java/com/estudo/dscatalog/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/estudo/dscatalog/service/impl/CategoryServiceImpl.java
@@ -2,14 +2,17 @@ package com.estudo.dscatalog.service.impl;
 
 import com.estudo.dscatalog.dto.request.CategoryRequestDTO;
 import com.estudo.dscatalog.dto.response.CategoryResponseDTO;
+import com.estudo.dscatalog.exception.exceptions.DatabaseException;
 import com.estudo.dscatalog.exception.exceptions.ResourceNotFoundException;
 import com.estudo.dscatalog.model.Category;
 import com.estudo.dscatalog.repository.CategoryRepository;
 import com.estudo.dscatalog.service.CategoryService;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -67,7 +70,7 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    @Transactional
+    @Transactional(propagation = Propagation.SUPPORTS)
     public void delete(Long id) {
         if(id == null || id <= 0){
             throw new RuntimeException("O id precisa ser positivo e não nulo");
@@ -75,7 +78,12 @@ public class CategoryServiceImpl implements CategoryService {
         if(!categoryRepository.existsById(id)){
             throw new ResourceNotFoundException("Recurso não encotrado");
         }
-        categoryRepository.deleteById(id);
+        try {
+            categoryRepository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
+        }
     }
 
     public static void toDTO(CategoryRequestDTO categoryRequestDTO, Category category){
